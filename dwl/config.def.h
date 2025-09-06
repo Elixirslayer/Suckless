7337868a833062c5eb7bc8514d9fecadc8812030
Elixirslayer <elixerslayer2.0@gmail.com>
2025-09-06 08:38:30 +0530

    config

--- dwl/config.def.h
+++ dwl/config.def.h
@@ -32,7 +32,7 @@ static int log_level = WLR_ERROR;
 
 /* Autostart */
 static const char *const autostart[] = {
-		"wbg", "-s", "/home/oc/wallpapers/0KF3QkdG.png", NULL,
+		// "wbg", "-s", "/home/oc/wallpapers/landscape7.jpg", NULL,
 		"dunst", NULL,
 		"waybar", NULL,
 		"input-remapper-control", "--command", "autoload", NULL,
@@ -143,14 +143,17 @@ static const double accel_speed = 0.0;
    */
 static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_LRM;
 
-/* If you want to use the windows key for MODKEY, use WLR_MODIFIER_LOGO */
-#define MODKEY WLR_MODIFIER_ALT
+/* If you want to use the windows key for Alt, use Super */
+#define Alt WLR_MODIFIER_ALT
+#define Ctrl WLR_MODIFIER_CTRL
+#define Shift WLR_MODIFIER_SHIFT
+#define Super WLR_MODIFIER_LOGO
 
 #define TAGKEYS(KEY,SKEY,TAG) \
-{ MODKEY,                    KEY,            view,            {.ui = 1 << TAG} }, \
-{ MODKEY|WLR_MODIFIER_CTRL,  KEY,            toggleview,      {.ui = 1 << TAG} }, \
-{ MODKEY|WLR_MODIFIER_SHIFT, SKEY,           tag,             {.ui = 1 << TAG} }, \
-{ MODKEY|WLR_MODIFIER_CTRL|WLR_MODIFIER_SHIFT,SKEY,toggletag, {.ui = 1 << TAG} }
+{ Alt,                    KEY,            view,            {.ui = 1 << TAG} }, \
+{ Alt|Ctrl,  KEY,            toggleview,      {.ui = 1 << TAG} }, \
+{ Alt|Shift, SKEY,           tag,             {.ui = 1 << TAG} }, \
+{ Alt|Ctrl|Shift,SKEY,toggletag, {.ui = 1 << TAG} }
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -162,63 +165,64 @@ static const char *menucmd[] = { "wofi", "--show", "drun", NULL };
 static const Key keys[] = {
 		/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
 		/* modifier                  key                 function        argument */
-		{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
-		{ MODKEY,                    XKB_KEY_b,          togglebar,      {0} },
-		{ MODKEY,					 XKB_KEY_f,		     spawn,          SHCMD("librewolf") },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_F,		     spawn,          SHCMD("qutebrowser") },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_E,		     spawn,          SHCMD("nautilus") },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_D,		     spawn,          SHCMD("discord") },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_W, 		 spawn, 		 SHCMD("setsid -w -f foot --app-id=wttr.in --hold -e curl wttr.in/456001") },
-		{ WLR_MODIFIER_LOGO,					 XKB_KEY_r,		     spawn,          SHCMD("/home/oc/Scripts/record") },
-		{ WLR_MODIFIER_LOGO,					 XKB_KEY_d,		     spawn,          SHCMD("/home/oc/Scripts/dictionaryW") },
-		{ WLR_MODIFIER_LOGO,					 XKB_KEY_n,		     spawn,          SHCMD("/home/oc/Scripts/note") },
+		{ Alt,                    XKB_KEY_b,          togglebar,      {0} },
+		{ Alt,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
+		{ Alt|Shift, XKB_KEY_Return,     spawn,          {.v = termcmd} },
+		{ Alt|Shift, XKB_KEY_F,		     spawn,          SHCMD("qutebrowser") },
+		{ Alt|Shift, XKB_KEY_E,		     spawn,          SHCMD("nautilus") },
+		{ Alt|Shift, XKB_KEY_D,		     spawn,          SHCMD("discord") },
+		{ Alt|Shift, XKB_KEY_W, 		 spawn, 		 SHCMD("setsid -w -f foot --app-id=wttr.in --hold -e curl wttr.in/456001") },
+		{ Super,					 XKB_KEY_r,		     spawn,          SHCMD("/home/oc/Scripts/record") },
+		{ Super,					 XKB_KEY_d,		     spawn,          SHCMD("/home/oc/Scripts/dictionaryW") },
+		{ Super,					 XKB_KEY_n,		     spawn,          SHCMD("/home/oc/Scripts/note") },
 		{ 0,					 XKB_KEY_Print,		     spawn,          SHCMD("/home/oc/Scripts/screenshot-fullW") },
-		{ WLR_MODIFIER_SHIFT,					 XKB_KEY_Print,		     spawn,          SHCMD("/home/oc/Scripts/screenshot-selectionW") },
-		{ WLR_MODIFIER_LOGO,					 XKB_KEY_e,		     spawn,          SHCMD("unipicker --command 'wofi --show dmenu' --copy-command 'wl-copy --type text/plain'") },	
-		{ MODKEY|WLR_MODIFIER_CTRL|WLR_MODIFIER_SHIFT, XKB_KEY_R,		     spawn,          SHCMD("reboot") },
-		{ MODKEY|WLR_MODIFIER_CTRL|WLR_MODIFIER_SHIFT, XKB_KEY_P,		     spawn,          SHCMD("poweroff") },
-		{ MODKEY|WLR_MODIFIER_CTRL|WLR_MODIFIER_SHIFT, XKB_KEY_S,		     spawn,          SHCMD("systemctl suspend") },
-		{ WLR_MODIFIER_LOGO,					 XKB_KEY_v,		     spawn,          SHCMD("cliphist list | wofi --show dmenu | cliphist decode | wl-copy") },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_W,		     spawn,          SHCMD("setsid -w -f foot --app-id=wttr.in --hold -e curl wttr.in/456001") },
+		{ Shift,					 XKB_KEY_Print,		     spawn,          SHCMD("/home/oc/Scripts/screenshot-selectionW") },
+		{ Super,					XKB_KEY_e,		     spawn,          SHCMD("wofi-emoji") },
+		{ Super,					 XKB_KEY_s,		     spawn,          SHCMD("unipicker --command 'wofi --show dmenu' --copy-command 'wl-copy --type text/plain'") },	
+		{ Alt|Ctrl|Shift, XKB_KEY_R,		     spawn,          SHCMD("reboot") },
+		{ Alt|Ctrl|Shift, XKB_KEY_P,		     spawn,          SHCMD("poweroff") },
+		{ Alt|Ctrl|Shift, XKB_KEY_S,		     spawn,          SHCMD("systemctl suspend") },
+		{ Super,					 XKB_KEY_v,		     spawn,          SHCMD("cliphist list | wofi --show dmenu | cliphist decode | wl-copy") },
+		{ Alt|Shift, XKB_KEY_W,		     spawn,          SHCMD("setsid -w -f foot --app-id=wttr.in --hold -e curl wttr.in/456001") },
+
 		/* Audio and Media Controls */
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F1,      spawn,          SHCMD("~/Suckless/dmenu/Scripts/dmenu_audioswitch") },
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F2,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5% && pkill -RTMIN+11 waybar") },
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F3,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5% && pkill -RTMIN+11 waybar") },
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F4,      spawn,          SHCMD("/home/oc/Scripts/media-play-pause.zsh") },
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F5,      spawn,          SHCMD("/home/oc/Scripts/media-previous.zsh") },
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F6,      spawn,          SHCMD("/home/oc/Scripts/media-next.zsh") },
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F7,      spawn,          SHCMD("/home/oc/Scripts/media-stop.zsh") },
-		{ WLR_MODIFIER_SHIFT,              XKB_KEY_F8,      spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle && pkill -RTMIN+11 waybar") },
+		{ Shift,              XKB_KEY_F1,      spawn,          SHCMD("~/Suckless/dmenu/Scripts/dmenu_audioswitch") },
+		{ Shift,              XKB_KEY_F2,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ -5% && pkill -RTMIN+11 waybar") },
+		{ Shift,              XKB_KEY_F3,      spawn,          SHCMD("pactl set-sink-volume @DEFAULT_SINK@ +5% && pkill -RTMIN+11 waybar") },
+		{ Shift,              XKB_KEY_F4,      spawn,          SHCMD("/home/oc/Scripts/media-play-pause.zsh") },
+		{ Shift,              XKB_KEY_F5,      spawn,          SHCMD("/home/oc/Scripts/media-previous.zsh") },
+		{ Shift,              XKB_KEY_F6,      spawn,          SHCMD("/home/oc/Scripts/media-next.zsh") },
+		{ Shift,              XKB_KEY_F7,      spawn,          SHCMD("/home/oc/Scripts/media-stop.zsh") },
+		{ Shift,              XKB_KEY_F8,      spawn,          SHCMD("pactl set-sink-mute @DEFAULT_SINK@ toggle && pkill -RTMIN+11 waybar") },
 
 		/* Microphone Volume Controls */
-		{ WLR_MODIFIER_SHIFT | WLR_MODIFIER_CTRL, XKB_KEY_F2, spawn, SHCMD("pactl set-source-volume @DEFAULT_SOURCE@ -5% && pkill -RTMIN+11 waybar") },
-		{ WLR_MODIFIER_SHIFT | WLR_MODIFIER_CTRL, XKB_KEY_F3, spawn, SHCMD("pactl set-source-volume @DEFAULT_SOURCE@ +5% && pkill -RTMIN+11 waybar") },
-		{ WLR_MODIFIER_SHIFT | WLR_MODIFIER_CTRL, XKB_KEY_F8, spawn, SHCMD("pactl set-source-mute @DEFAULT_SOURCE@ toggle && pkill -RTMIN+11 waybar") },
-		{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
-		{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_J,          movestack,      {.i = +1} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_K,          movestack,      {.i = -1} },
-		{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
-		{ MODKEY,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
-		{ MODKEY,                    XKB_KEY_h,          setmfact,       {.f = -0.05f} },
-		{ MODKEY,                    XKB_KEY_l,          setmfact,       {.f = +0.05f} },
-		{ MODKEY,                    XKB_KEY_Return,     zoom,           {0} },
-		{ MODKEY,                    XKB_KEY_Tab,        view,           {0} },
-		{ MODKEY,                    XKB_KEY_g,          togglegaps,     {0} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_C,          killclient,     {0} },
-		{ MODKEY,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
-		{ MODKEY,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
-		{ MODKEY,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
-		{ MODKEY,                    XKB_KEY_space,      setlayout,      {0} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_space,      togglefloating, {0} },
-		{ MODKEY,                    XKB_KEY_e,         togglefullscreen, {0} },
-		{ MODKEY,                    XKB_KEY_0,          view,           {.ui = ~0} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_parenright, tag,            {.ui = ~0} },
-		{ MODKEY,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
-		{ MODKEY,                    XKB_KEY_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_less,       tagmon,         {.i = WLR_DIRECTION_LEFT} },
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_greater,    tagmon,         {.i = WLR_DIRECTION_RIGHT} },
+		{ Shift | Ctrl, XKB_KEY_F2, spawn, SHCMD("pactl set-source-volume @DEFAULT_SOURCE@ -5% && pkill -RTMIN+11 waybar") },
+		{ Shift | Ctrl, XKB_KEY_F3, spawn, SHCMD("pactl set-source-volume @DEFAULT_SOURCE@ +5% && pkill -RTMIN+11 waybar") },
+		{ Shift | Ctrl, XKB_KEY_F8, spawn, SHCMD("pactl set-source-mute @DEFAULT_SOURCE@ toggle && pkill -RTMIN+11 waybar") },
+		{ Alt,                    XKB_KEY_j,          focusstack,     {.i = +1} },
+		{ Alt,                    XKB_KEY_k,          focusstack,     {.i = -1} },
+		{ Alt|Shift, XKB_KEY_J,          movestack,      {.i = +1} },
+		{ Alt|Shift, XKB_KEY_K,          movestack,      {.i = -1} },
+		{ Alt,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
+		{ Alt,                    XKB_KEY_d,          incnmaster,     {.i = -1} },
+		{ Alt,                    XKB_KEY_h,          setmfact,       {.f = -0.05f} },
+		{ Alt,                    XKB_KEY_l,          setmfact,       {.f = +0.05f} },
+		{ Alt,                    XKB_KEY_Return,     zoom,           {0} },
+		{ Alt,                    XKB_KEY_Tab,        view,           {0} },
+		{ Alt,                    XKB_KEY_g,          togglegaps,     {0} },
+		{ Alt|Shift, XKB_KEY_C,          killclient,     {0} },
+		{ Alt,                    XKB_KEY_t,          setlayout,      {.v = &layouts[0]} },
+		{ Alt,                    XKB_KEY_f,          setlayout,      {.v = &layouts[1]} },
+		{ Alt,                    XKB_KEY_m,          setlayout,      {.v = &layouts[2]} },
+		{ Alt,                    XKB_KEY_space,      setlayout,      {0} },
+		{ Alt|Shift, XKB_KEY_space,      togglefloating, {0} },
+		{ 0,	                     XKB_KEY_F11,         togglefullscreen, {0} },
+		{ Alt,                    XKB_KEY_0,          view,           {.ui = ~0} },
+		{ Alt|Shift, XKB_KEY_parenright, tag,            {.ui = ~0} },
+		{ Alt,                    XKB_KEY_comma,      focusmon,       {.i = WLR_DIRECTION_LEFT} },
+		{ Alt,                    XKB_KEY_period,     focusmon,       {.i = WLR_DIRECTION_RIGHT} },
+		{ Alt|Shift, XKB_KEY_less,       tagmon,         {.i = WLR_DIRECTION_LEFT} },
+		{ Alt|Shift, XKB_KEY_greater,    tagmon,         {.i = WLR_DIRECTION_RIGHT} },
 		TAGKEYS(          XKB_KEY_1, XKB_KEY_exclam,                     0),
 		TAGKEYS(          XKB_KEY_2, XKB_KEY_at,                         1),
 		TAGKEYS(          XKB_KEY_3, XKB_KEY_numbersign,                 2),
@@ -228,20 +232,20 @@ static const Key keys[] = {
 		TAGKEYS(          XKB_KEY_7, XKB_KEY_ampersand,                  6),
 		TAGKEYS(          XKB_KEY_8, XKB_KEY_asterisk,                   7),
 		TAGKEYS(          XKB_KEY_9, XKB_KEY_parenleft,                  8),
-		{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Q,          quit,           {0} },
+		{ Alt|Shift, XKB_KEY_Q,          quit,           {0} },
 
 		/* Ctrl-Alt-Backspace and Ctrl-Alt-Fx used to be handled by X server */
-		{ WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_Terminate_Server, quit, {0} },
+		{ Ctrl|Alt,		XKB_KEY_Terminate_Server, quit, {0} },
 		/* Ctrl-Alt-Fx is used to switch to another VT, if you don't know what a VT is
 		 * do not remove them.
 		 */
-#define CHVT(n) { WLR_MODIFIER_CTRL|WLR_MODIFIER_ALT,XKB_KEY_XF86Switch_VT_##n, chvt, {.ui = (n)} }
+#define CHVT(n) { Ctrl|WLR_MODIFIER_ALT,XKB_KEY_XF86Switch_VT_##n, chvt, {.ui = (n)} }
 		CHVT(1), CHVT(2), CHVT(3), CHVT(4), CHVT(5), CHVT(6),
 		CHVT(7), CHVT(8), CHVT(9), CHVT(10), CHVT(11), CHVT(12),
 };
 
 static const Button buttons[] = {
-		{ MODKEY, BTN_LEFT,   moveresize,     {.ui = CurMove} },
-		{ MODKEY, BTN_MIDDLE, togglefloating, {0} },
-		{ MODKEY, BTN_RIGHT,  moveresize,     {.ui = CurResize} },
+		{ Alt, BTN_LEFT,   moveresize,     {.ui = CurMove} },
+		{ Alt, BTN_MIDDLE, togglefloating, {0} },
+		{ Alt, BTN_RIGHT,  moveresize,     {.ui = CurResize} },
 };
